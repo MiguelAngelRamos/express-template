12ae18e9aa0e5ceda5918c602ede75934416f267
Miguel Angel Ramos <return.infox@gmail.com>
2024-01-29 16:01:32 -0300

    creamos las rutas y establecimos template engine

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 // CommonJS, ES6 con los imports
 const express = require('express'); // CommonJS
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
+
+// Establecer EJS como template engine (motor de plantillas)
+app.set('view engine', 'ejs');
 // Requerir Rutas
 const indexRouter = require('./routes/index');
-
+const usersRouter = require('./routes/users');
 // Requerir Rutas
-
+// localhost:3000
+app.use('/', indexRouter);
+// http://localhost:3000/users/
+// http://localhost:3000/users
+app.use('/users', usersRouter)
 
 
 // Configurar lo que son los layouts
