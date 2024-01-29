b22b72dcccd4eea017f66000ea54c71889e989c7
Miguel Angel Ramos <return.infox@gmail.com>
2024-01-29 16:54:34 -0300

    partials

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const express = require('express'); // CommonJS
 const expressLayouts = require('express-ejs-layouts');
 const app = express();
 
-// Establecer EJS como template engine (motor de plantillas)
-app.set('view engine', 'ejs');
 // Requerir Rutas
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-// Requerir Rutas
+
+// Establecer EJS como template engine (motor de plantillas)
+app.set('view engine', 'ejs');
+
+// Configurar el express-ejs-layouts
+app.use(expressLayouts); // Aseguramos que este layout venga antes de cualquier app.use('/')
+app.set('layout', 'layouts/layout');
 // localhost:3000
 app.use('/', indexRouter);
 // http://localhost:3000/users/
@@ -18,6 +22,9 @@ app.use('/users', usersRouter)
 
 // Configurar lo que son los layouts
 
+//* El path de los estaticos http://localhost:3000/public
+// Estaticos
+app.use('/public', express.static('public'));
 
 // Iniciar el Servidor
 app.listen(3000, () => {
